8c7ff05e0fe25153eb60fa40d0ff735683df67d0
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-05-31 03:49:24 +0530

    Add files via upload

---
+++ collectionframeworkLL.java
@@ -0,0 +1,53 @@
+
+//LINKEDLIST
+import java.util.*;
+
+public class collectionframeworkLL {
+    
+    static void LinkedListExample(){
+           LinkedList<Integer> l =new LinkedList<>();
+          
+           l.add(1);
+           l.add(2);
+           l.add(5);
+
+           System.out.println(l);
+
+           l.add(1,12);
+           l.add(4,19);
+           
+           System.out.println(l);
+
+
+        System.out.println( l.get(2));
+
+        l.set(2,10);
+        l.set(0,4);
+
+        System.out.println(l);
+
+        System.out.println(l.size());
+
+        l.sort(null);
+        System.out.println(l);
+
+        
+        System.out.println(l.contains(10));
+
+        l.remove(3);
+        System.out.println(l);
+
+        l.clear();
+        System.out.println(l);
+    
+        }
+
+
+
+
+    public static void main(String[]args){
+
+        LinkedListExample();
+    }
+
+}
