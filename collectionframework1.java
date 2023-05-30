27b38b044aa2f819d4552df7e3502d05e33c5291
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-05-30 23:46:34 +0530

    Add files via upload

---
+++ collectionframework1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class collectionframework1{
+
+    static void  ArrayListExamples(){
+        ArrayList<Integer> l= new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+
+        l.add(2,15);//adding element at specific index.
+
+        
+        System.out.println(l);
+        System.out.println(l.indexOf(3)); //returns the index of the element inserted
+       
+
+       System.out.println( l.get(1)); //to retreive an element
+         l.set(1,5);  //to modify element at specific index
+          System.out.println(l);
+
+
+          System.out.println(l.contains(10));  //false bcz 10 is not present.
+             
+           System.out.println(l.size()); //returns the size of the arraylist
+         
+        l.sort(null); // for sorting the arraylist elements
+            System.out.println(l);
+
+            l.ensureCapacity(5); //Increases the capacity of this ArrayList instance, if necessary, to ensure that it can hold at least the number of elements specified by the minimum capacity argument
+            System.out.println(l.isEmpty()); // to check whether arraylist is empty or not.
+        }  
+    
+    public static void main(String[]args){
+        ArrayListExamples();
+    }
+}
