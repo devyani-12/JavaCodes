779d6773d59c7fd40ed1419436d9346a04eda95a
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-06-01 01:48:17 +0530

    Add files via upload

---
+++ collectionframeworktreeset.java
@@ -0,0 +1,19 @@
+//3rd variation of set---->TreeSet----> chahe kisi b order me daale elements treeset will always return elements
+                                              //in ascending (sorted) order.
+                                              //--> uses Binary Search Tree(particularly red-black tree)
+                                              //least efficient among all 3 variations. 
+                                              //insertion n retreival complexity O(log n)
+
+import java.util.*;
+public class collectionframeworktreeset {
+    static void TreeSet(){
+ 
+
+
+    }
+
+    public static void main(String[]args) {
+
+        TreeSet();
+    }
+}
