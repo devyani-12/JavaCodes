3406e5a4691ff6d7fefa6a2a884b4e1c27a53e40
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-05-31 23:30:09 +0530

    Update collectionframeworkstack.java

--- collectionframeworkstack.java
+++ collectionframeworkstack.java
@@ -10,22 +10,27 @@ public class collectionframeworkstack {
 
         s.push("hi");
         s.push("Hello");
+        s.push("abc");
+        s.push("bc");
+        s.push("ab");
+        s.push("a");
 
          System.out.println(s.peek()); //will return the topmost element present in stack.
-                 //hello will be the output
+                 //a will be the output
          System.out.println(s.pop());//it will return as well as remove the topmost element of stack.
-                // hello will be removed.
+                // a will be removed.
 
-        System.out.println(s.peek()); // now since hello has been removed in prev line so 
-                                        //output should be hi.
-          System.out.println(s.size());
-        
-        
-             System.out.println(s.search("abc"));     
+        System.out.println(s.peek()); // now since a has been removed in prev line so 
+                                        //output should be ab.
+          System.out.println(s.size()); // 5 since a is removed.
+
+          System.out.println(s.search("abc")); //return the distance of abc from the topmost element present in stack
+                                                      // since a is already removed using pop.now ab is topmost 
+                                                           //distance of abc from ab is 3 ..so output is 3.
           
           System.out.println(s.isEmpty()); //false
             
-        System.out.println(s.contains("abc")); //false
+        System.out.println(s.contains("abc")); //true
 
           s.clear();
 
@@ -36,18 +41,7 @@ public class collectionframeworkstack {
          
 
     }
-   
-
-
-   
-    
-
-
-
-
-
-
-  public static void main(String[]args){
+   public static void main(String[]args){
     Stack();
   }  
 }
